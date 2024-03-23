58b778a1fef8f4b20488344d981971186c6baca3
poriz <areacmzl@gmail.com>
2024-03-23 11:31:05 +0900

    variables

--- 01-docker-terraform/terraform_gcp/main.tf
+++ 01-docker-terraform/terraform_gcp/main.tf
@@ -1,21 +1,24 @@
 terraform {
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = "5.21.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.21.0"
     }
+  }
 }
 
 provider "google" {
-    # gcloud auth application-default login
-    project = "<Your Project ID>" 
-    region  = "asia-northeast3" 
+  # gcloud auth application-default login
+  # project = "<Your Project ID>" 
+  credentials = var.credentials
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
-  location      = "ASIA"
+  # name          = "<Your Unique Bucket Name>"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -26,4 +29,8 @@ resource "google_storage_bucket" "data-lake-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
